77fc69d6ddbade53ee3161acd9255ae45c50b66b
Ahmed AL Awamleh <awamlehad@icloud.com>
2022-08-06 21:58:33 +0300

    task8-api

--- server.js
+++ server.js
@@ -1,46 +1,85 @@
-const express=require(`express`);
+const express = require(`express`);
 require(`dotenv`).config();
-const cors=require(`cors`);
-
-const app =express();
+const cors = require(`cors`);
+const axios = require(`axios`)
+const app = express();
 app.use(cors());
+const port = process.env.PORT;
+const weatherData = require(`./data/weather.json`);
 
-const weatherData=require(`./data/weather.json`);
-const { NavItem } = require("react-bootstrap");
-app.get(`/weather`,(req,res)=>{
-    const searchQuery=req.query.searchQuery;
-    const lat=req.query.lat;
-    const lon =req.query.lon;
+app.get("/weather", async (req, res) => {
+    const searchQuery = req.query.searchQuery;
+    const lat = req.query.lat;
+    const lon = req.query.lon;
+    //WEATHER_API_KEY
+    const cityArry = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`)
 
-   
-    const cityArry=weatherData.find(item=>item.city_name.toLocaleLowerCase() === searchQuery.toLocaleLowerCase())
-    try{
-        const cityData=cityArry.data.map(item => new Forecast(item))
-console.log(cityData)
-res.send({cityData});
-    }catch(error){
+    console.log(cityArry.data)
+    const result = []
+
+    try {
+        cityArry.data.data.map(item => {
+            result.push(new Forecast(item))
+        })
+
+        res.status(200).send(result);
+    } catch (error) {
         errorMes(error, res)
     }
-    app.get(`*`,(req,res)=>{res.status(404).send(`not found`)})
 
+
+
+
+
+
+
+})
+
+app.get("/movies", async (req, res) => {
+    const searchQuery = req.query.searchQuery;
+    const movieArr = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`)
+
+    try {
+        const movieData = movieArr.data.results.map(item => new movie(item));
+        res.status(200).send(movieData)
+
+    } catch (error) {
+        errorMes(error, res)
+    }
 })
-class Forecast{
-    constructor(day){
-    this.date=day.valid_date;
-    this.description=day.weather.description;
+
+
+class movie {
+    constructor(movie) {
+        this.title = movie.title;
+        this.overview = movie.overview;
+        this.avearge_votes = movie.vote_avearge;
+        this.image_url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+        this.popularity = movie.popularity;
+        this.released_on = movie.released_date;
+        this.total_votes = movie.vote_count;
     }
 }
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is Working`)
-}
-    )
 
-    function errorMes(error,res){
-res.status(500).send({error:`wrong Input`})
+class Forecast {
+    constructor(day) {
+        this.date = day.valid_date;
+        this.description = day.weather.description;
     }
+}
+app.listen(3002, () => {
+    console.log(`Server is Working `)
+}
+)
+
+function errorMes(error, res) {
+    res.status(500).send({ error: `wrong Input` })
+}
 
 
 
+
+app.get(`*`, (req, res) => { res.status(404).send(`not found`) })
 
 
 
